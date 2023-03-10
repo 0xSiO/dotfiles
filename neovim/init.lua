7d8a33ca553f3f48cbcd1aa16f4ab8cfb04e92c3
Luc Street <6709262+0xSiO@users.noreply.github.com>
2023-03-10 00:24:07 -0800

    Disable netrw

--- neovim/init.lua
+++ neovim/init.lua
@@ -4,6 +4,10 @@ vim.g.loaded_ruby_provider = 0
 vim.g.loaded_perl_provider = 0
 vim.g.loaded_node_provider = 0
 
+-- Disable netrw
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
 -- Install lazy.nvim
 local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
