ff81ff1600f8f191e27ab26f8244c16b922279ab
Luc Street <6709262+0xSiO@users.noreply.github.com>
2024-01-08 17:16:30 -0800

    Skip vaults in backup script, tweak zshrc

--- linux/backup.sh
+++ linux/backup.sh
@@ -2,7 +2,6 @@
 # Use restic to backup important files to external location
 
 BACKUP_LOCATION="/run/media/luc/Seagate Backup+"
-# BACKUP_LOCATION="/home/luc/pCloudDrive"
 
 if [ ! -d "$BACKUP_LOCATION" ]; then
     echo "Backup location not found: $BACKUP_LOCATION"
@@ -11,19 +10,20 @@ fi
 
 echo "Starting backup."
 restic -r "$BACKUP_LOCATION" backup --verbose ~/ \
-    -e ~/pCloudDrive \
     -e ~/.local/share/Trash \
     -e ~/.local/share/containers \
-    -e ~/.local/share/JetBrains \
     -e ~/.asdf \
     -e ~/.cache \
     -e ~/.cargo \
     -e ~/.gradle \
     -e ~/.npm \
-    -e ~/.pcloud \
     -e ~/.rustup \
     -e ~/.venvs \
     -e "node_modules" \
     -e "target/debug" \
     -e "target/doc" \
-    -e "target/release"
+    -e "target/release" \
+    -e ~/MEGA/Characters \
+    -e ~/MEGA/Documents \
+    -e ~/MEGA/Exports \
+    -e ~/Downloads/Vault
