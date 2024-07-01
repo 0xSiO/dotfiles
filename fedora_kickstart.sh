839bb5aa25307c760f61b3e297df823342b4027a
Luc Street <6709262+0xSiO@users.noreply.github.com>
2024-06-30 22:14:37 -0700

    Updates for fresh Fedora 40 install

--- fedora_kickstart.sh
+++ fedora_kickstart.sh
@@ -8,7 +8,7 @@ fi
 export HOME="/home/$SUDO_USER"
 
 echo -e "\n=== Removing unnecessary packages ==="
-dnf remove -y gnome-boxes gnome-calendar gnome-clocks gnome-contacts gnome-maps gnome-photos gnome-weather cheese
+dnf remove -y gnome-boxes gnome-calendar gnome-clocks gnome-contacts gnome-maps gnome-weather
 
 echo -e "\n=== Updating system and installing essential packages ==="
 dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
@@ -22,10 +22,10 @@ dnf swap -y ffmpeg-free ffmpeg --allowerasing
 dnf install -y intel-media-driver
 
 # Basic goodies
-dnf install -y anacron aria2 autojump-zsh bat cmake eza fd-find ffmpeg-devel ffmpegthumbnailer file-roller \
-    file-roller-nautilus firewall-config gcc-c++ git-delta gnome-tweaks htop kernel-devel libpq-devel megasync \
-    mpv ncdu neovim numix-icon-theme-circle postfix postgresql-server postgresql-contrib puddletag pv restic \
-    ripgrep tokei transmission zsh
+dnf install -y alacritty aria2 autojump-zsh bat clang cronie-anacron eza fd-find ffmpegthumbnailer \
+    file-roller file-roller-nautilus firewall-config git-delta gnome-tweaks htop mpv ncdu neovim \
+    numix-icon-theme-circle postfix postgresql-server postgresql-contrib puddletag pv restic \
+    ripgrep s-nail tokei tmux transmission zsh
 
 echo -e "\n=== Miscellaneous configuration ==="
 
@@ -71,9 +71,6 @@ newaliases
 systemctl enable postfix
 systemctl start postfix
 
-echo -e "\n=== Installing asdf ==="
-git clone https://github.com/asdf-vm/asdf.git ~/.asdf
-
 echo -e "\n=== Downloading dotfiles ==="
 rm -rf ~/.dotfiles_old
 git clone --single-branch --branch master --recursive https://github.com/0xSiO/dotfiles ~/.dotfiles
@@ -81,23 +78,30 @@ git clone --single-branch --branch master --recursive https://github.com/0xSiO/d
 mkdir /etc/cron.daily
 ln -sf ~/.dotfiles/linux/update_tools.sh /etc/cron.daily/update-tools
 
+echo -e "\n=== Installing asdf ==="
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf
 source ~/.asdf/asdf.sh
 
+echo -e "\n=== Installing rust ==="
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --no-modify-path --default-host $(arch)-unknown-linux-gnu --default-toolchain stable -c rust-src
+source ~/.cargo/env
+dnf install -y openssl-devel
+cargo install cargo-audit cargo-outdated cargo-update
+
 echo -e "\n=== Installing latest stable ruby ==="
-dnf install -y autoconf patch make bzip2 openssl-devel libyaml-devel libffi-devel readline-devel zlib-devel gdbm-devel ncurses-devel
+dnf install -y autoconf gcc patch make bzip2 openssl-devel libyaml-devel libffi-devel readline-devel zlib-devel gdbm-devel ncurses-devel
 asdf plugin add ruby
 asdf install ruby latest
 asdf global ruby latest
 
 if [ $? -eq 0 ]; then
-    gem install solargraph
     asdf reshim
 else
-    exit 3
+    exit 2
 fi
 
 echo -e "\n=== Installing latest stable python / poetry ==="
-dnf install -y make patch zlib-devel bzip2 bzip2-devel readline-devel sqlite sqlite-devel openssl-devel tk-devel libffi-devel xz-devel libuuid-devel gdbm-libs libnsl2
+dnf install -y make gcc patch zlib-devel bzip2 bzip2-devel readline-devel sqlite sqlite-devel openssl-devel tk-devel libffi-devel xz-devel libuuid-devel gdbm-libs libnsl2
 asdf plugin add python
 asdf install python latest
 asdf global python latest
@@ -108,7 +112,7 @@ if [ $? -eq 0 ]; then
     asdf reshim
     poetry config virtualenvs.path ~/.venvs
 else
-    exit 4
+    exit 3
 fi
 
 echo -e "\n=== Installing latest nodejs ==="
@@ -120,15 +124,9 @@ if [ $? -eq 0 ]; then
     npm install -g npm
     asdf reshim
 else
-    exit 5
+    exit 4
 fi
 
-echo -e "\n=== Installing rust ==="
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --no-modify-path --default-host $(arch)-unknown-linux-gnu --default-toolchain stable -c rust-src
-source ~/.cargo/env
-cargo install cargo-audit cargo-outdated cargo-update
-
 chown -hR $SUDO_USER:$SUDO_USER ~/
-rm -rf /tmp/*
 
 echo -e "\n=== All set up! ==="
