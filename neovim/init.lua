3491afa154b06a75889291981e1f7b77be822c0d
Luc Street <6709262+0xSiO@users.noreply.github.com>
2025-06-04 13:32:57 -0700

    Use system clipboard, sort other options

--- neovim/init.lua
+++ neovim/init.lua
@@ -355,23 +355,24 @@ require('lazy').setup({
 })
 
 -- Other options
-vim.o.signcolumn = 'yes'
-vim.o.winborder = 'rounded'
+vim.o.expandtab = true
+vim.o.hidden = false
+vim.o.ignorecase = true
 vim.o.number = true
 vim.o.relativenumber = true
-vim.o.splitright = true
-vim.o.splitbelow = true
-vim.o.undofile = true
-vim.o.hidden = false
-vim.o.updatetime = 400
-vim.o.ignorecase = true
-vim.o.smartcase = true
-vim.o.showmode = false
-vim.o.expandtab = true
-vim.o.tabstop = 4
 vim.o.shiftwidth = 4
-vim.opt.diffopt:append({ 'algorithm:histogram' })
-vim.opt.fillchars:append({ diff = ' ' })
+vim.o.showmode = false
+vim.o.signcolumn = 'yes'
+vim.o.smartcase = true
+vim.o.splitbelow = true
+vim.o.splitright = true
+vim.o.tabstop = 4
+vim.o.undofile = true
+vim.o.updatetime = 400
+vim.o.winborder = 'rounded'
+vim.opt.clipboard:append('unnamedplus')
+vim.opt.diffopt:append('algorithm:histogram')
+vim.opt.fillchars:append('diff: ')
 
 -- Other keybindings
 vim.keymap.set('n', '<leader>n', function()
